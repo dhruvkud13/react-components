316e13d90ad461c8c2e86d49d8163c286a6efbbc
dhruvkud13 <dhruvkudalkar13@gmail.com>
2022-07-26 11:55:47 +0530

    login done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import './App.css';
 import FormInput from './components/FormInput';
-
 function App() {
   const[values,setValues]=useState({
     username:'',
@@ -10,17 +9,15 @@ function App() {
     password:'',
     confirmPassword:''
   })
-  // use useRef to prevent re rendering
-  // const usernameRef = useRef();
   const inputs=[
     {
       id:1,
-      name:'username',
+      name:'SAPID',
       type:'text',
-      placeholder:'Username',
-      errorMessage:"Username should be 3-16 characters and shouldn't include any special character!",
-      label:'Username',
-      pattern: "^[A-Za-z0-9]{3,16}$",
+      placeholder:'SAPID',
+      errorMessage:"SAPID should be 10 digits and shouldn't include any other characters!",
+      label:'SAPID',
+      pattern: "^[0-9]{10}$",
       required:true,
     },
     {
@@ -34,39 +31,18 @@ function App() {
     },
     {
       id: 3,
-      name: "birthday",
-      type: "date",
-      placeholder: "Birthday",
-      label: "Birthday",
-    },
-    {
-      id: 4,
       name: "password",
       type: "password",
       placeholder: "Password",
-      errorMessage:"Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      errorMessage:"Password should be 10 characters and include at least 1 letter, 1 number and 1 special character!",
       label: "Password",
       pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
       required: true,
     },
-    {
-      id: 5,
-      name: "confirmPassword",
-      type: "password",
-      placeholder: "Confirm Password",
-      errorMessage: "Passwords don't match!",
-      label: "Confirm Password",
-      pattern: values.password,
-      required: true,
-    },
   ]
-  
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // console.log(usernameRef);
-    // const data= new FormData(e.target);
-    // console.log(Object.fromEntries(data.entries()));
   }
 
   const onChange=(e)=>{
@@ -79,7 +55,7 @@ function App() {
   return (
     <div className='app'>
       <form onSubmit={handleSubmit}>
-        <h1>REGISTER</h1>
+        <h1>LOGIN</h1>
         {inputs.map((input)=>(
           <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange} />
         ))}
